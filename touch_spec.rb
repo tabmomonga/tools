ca9f7947676ee8815a70823c1fa7eca7ebe0ec4c
zaki <zaki@04999e60-30d4-0310-a325-f3709f8735f4>
2004-02-29 05:07:42 +0000

    add touch_spec.rb
    git-svn-id: http://svn.momonga-linux.org/svn/pkgs/trunk/tools@26 04999e60-30d4-0310-a325-f3709f8735f4

---
+++ touch_spec.rb
@@ -0,0 +1,48 @@
+#! /usr/bin/env ruby
+require 'getoptlong'
+
+$:.unshift(File.dirname($0))
+require 'environment'
+load 'updatespecdb'
+
+if File.expand_path($PKGDIR) != File.expand_path(Dir.getwd)
+  puts "Run in pkgs/ dir."
+  exit 1
+end
+
+class Dir
+  def Dir.in_dir(dir)
+    oldpwd = Dir.pwd
+    Dir.chdir(dir)
+    yield
+    Dir.chdir(oldpwd)
+  end
+end # class Dir
+
+db = SpecDB.new(true)
+
+db.names.each do |name|
+  spec = db.specs[name]
+  nosources = spec.sources.select{|s| s.no?}
+  topdir = $TOPDIR.dup
+
+  Dir.in_dir(name) do
+    if File.exist?('TO.Zoo') then
+      topdir += '-Zoo'
+    elsif File.exist?('TO.Nonfree') then
+      topdir += '-Nonfree'
+    end
+    srpm = "#{topdir}/SRPMS/#{spec.name}-#{spec.packages[0].version}"
+    if nosources.empty?
+      srpm += ".src.rpm"
+    else
+      srpm += ".nosrc.rpm"
+    end
+    puts srpm
+    if File.exist?(srpm)
+      system("touch -r #{srpm} #{name}.spec")
+      puts name
+    end
+  end
+end
+
