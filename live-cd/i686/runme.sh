20dbfff9a6fea31e89ff6436c14334950e9caca3
mutecat <mutecat@04999e60-30d4-0310-a325-f3709f8735f4>
2005-08-19 21:29:42 +0000

    1st import orz
    git-svn-id: http://svn.momonga-linux.org/svn/pkgs/trunk/tools@6651 04999e60-30d4-0310-a325-f3709f8735f4

---
+++ live-cd/i686/runme.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# run this script to create a LiveCD in /tmp/livecd.iso
+# Your kernel image has to be in $ROOT/boot/vmlinuz or $ROOT/vmlinuz
+# 
+
+export PATH=.:./tools:../tools:/usr/sbin:/usr/bin:/sbin:/bin:/
+
+CHANGEDIR="`dirname \`readlink -f $0\``"
+echo "Changing current directory to $CHANGEDIR"
+cd $CHANGEDIR
+
+. liblinuxlive || exit 1
+. config || exit 1
+
+./install $ROOT
+
+VMLINUZ=$ROOT/boot/vmlinuz
+#VMLINUZ=$ROOT/boot/vmlinuz-2.6.10-41m
+if [ -L "$VMLINUZ" ]; then VMLINUZ=`readlink -f $VMLINUZ`; fi
+if [ "`ls $VMLINUZ 2>/dev/null`" = "" ]; then echo "cannot find $VMLINUZ"; exit 1; fi
+
+header "Creating LiveCD from your Linux"
+
+mkdir -p $CDDATA/base
+mkdir -p $CDDATA/modules
+mkdir -p $CDDATA/optional
+mkdir -p $CDDATA/rootcopy
+
+echo "copying cd-root to $CDDATA, using kernel from $VMLINUZ"
+echo "Using kernel modules from /lib/modules/$KERNEL"
+cp -R cd-root/* $CDDATA
+cp -R tools $CDDATA
+cp -R info/* $CDDATA
+cp $VMLINUZ $CDDATA/boot/vmlinuz
+#cp $VMLINUZ $CDDATA/boot/vmlinuz-2.6.10-41m
+
+echo "creating initrd image..."
+cd initrd
+./initrd_create
+if [ "$?" -ne 0 ]; then exit; fi
+cd ..
+
+cp initrd/$INITRDIMG.gz $CDDATA/boot/initrd.gz
+rm initrd/$INITRDIMG.gz
+
+echo "creating compressed images..."
+
+for dir in bin etc home lib opt root usr sbin var; do
+    if [ -d $ROOT/$dir ]; then
+      echo "base/$dir.mo"
+      create_module $ROOT/$dir $CDDATA/base/$dir.mo -keep-as-directory
+      if [ $? -ne 0 ]; then exit; fi
+    fi
+done
+
+echo "creating LiveCD ISO image..."
+cd $CDDATA
+./make_iso.sh /tmp/livecd.iso
+
+cd /tmp
+header "Your ISO is created in /tmp/livecd.iso"
