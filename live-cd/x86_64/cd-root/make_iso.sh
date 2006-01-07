10d055486eee58043cd37fa11cd4ad1094095624
dai <dai@04999e60-30d4-0310-a325-f3709f8735f4>
2006-01-07 11:48:11 +0000

    i686: fix pkglist i686: need usbcore module for 2.6.14 x86_64: 1st release
    git-svn-id: http://svn.momonga-linux.org/svn/pkgs/trunk/tools@7701 04999e60-30d4-0310-a325-f3709f8735f4

---
+++ live-cd/x86_64/cd-root/make_iso.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# ---------------------------------------------------
+# Script to create bootable ISO in Linux
+# usage: make_iso.sh /tmp/slax.iso
+# author: Tomas M. <http://www.linux-live.org>
+# ---------------------------------------------------
+
+CDLABEL="MOMONGA-LIVECD"
+
+if [ "$1" = "" -o "$1" = "--help" -o "$1" = "-h" ]; then
+  echo "This script will create bootable ISO from files in curent directory."
+  echo "Current directory must be writable."
+  echo "example: $0 /mnt/hda5/slax.iso"
+  exit
+fi
+
+# isolinux.bin is changed during the ISO creation,
+# so we need to restore it from backup.
+cp -f boot/isolinux.bi_ boot/isolinux.bin
+if [ $? -ne 0 ]; then
+   echo "Can't recreate isolinux.bin, make sure your current directory is writable!"
+   exit 1
+fi
+
+mkisofs -o "$1" -v -J -R -D -A "$CDLABEL" -V "$CDLABEL" \
+-no-emul-boot -boot-info-table -boot-load-size 4 \
+-b boot/isolinux.bin -c boot/isolinux.boot .
---
+++ live-cd/x86_64/runme.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#
+# run this script to create a LiveCD in /tmp/livecd.iso
+# Your kernel image has to be in $ROOT/boot/vmlinuz or $ROOT/vmlinuz
+# 
+
+if [ ! $# = 3 ]; then
+	echo "$0: [live env root dir] [kernel version] [output iso image]"
+	exit 1
+fi
+
+export ROOT=$1
+export KERNEL=$2
+ISOFILE=$3
+
+echo "cleanup yum cache"
+rm -rf $ROOT/var/cache/yum/*
+
+export PATH=.:./tools:../tools:/usr/sbin:/usr/bin:/sbin:/bin:/
+
+CHANGEDIR="`dirname \`readlink -f $0\``"
+echo "Changing current directory to $CHANGEDIR"
+cd $CHANGEDIR
+
+. liblinuxlive || exit 1
+. config || exit 1
+
+./install $ROOT
+
+VMLINUZ=$ROOT/boot/vmlinuz-$KERNEL
+if [ -L "$VMLINUZ" ]; then VMLINUZ=`readlink -f $VMLINUZ`; fi
+if [ "`ls $VMLINUZ 2>/dev/null`" = "" ]; then echo "cannot find $VMLINUZ"; exit 1; fi
+
+header "Creating LiveCD from your Linux"
+
+mkdir -p $CDDATA/base
+mkdir -p $CDDATA/modules
+mkdir -p $CDDATA/optional
+mkdir -p $CDDATA/rootcopy
+
+echo "copying cd-root to $CDDATA, using kernel from $VMLINUZ"
+echo "Using kernel modules from $ROOT/lib/modules/$KERNEL"
+cp -R cd-root/* $CDDATA
+cp -R tools $CDDATA
+cp -R info/* $CDDATA
+cp $VMLINUZ $CDDATA/boot/vmlinuz
+
+echo "creating initrd image..."
+cd initrd
+./initrd_create
+if [ "$?" -ne 0 ]; then exit; fi
+cd ..
+
+cp initrd/$INITRDIMG.gz $CDDATA/boot/initrd.gz
+rm initrd/$INITRDIMG.gz
+
+echo "creating compressed images..."
+
+for dir in bin etc home lib lib64 opt root usr sbin var; do
+    if [ -d $ROOT/$dir ]; then
+      echo "base/$dir.mo"
+      create_module $ROOT/$dir $CDDATA/base/$dir.mo -keep-as-directory
+      if [ $? -ne 0 ]; then exit; fi
+    fi
+done
+
+echo "creating LiveCD ISO image..."
+cd $CDDATA
+./make_iso.sh $ISOFILE
+
+cd /tmp
+header "Your ISO is created in $ISOFILE"
+
