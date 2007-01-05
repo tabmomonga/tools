358bc81da1ff26650fcf341d06240920fdded534
futoshi <futoshi@04999e60-30d4-0310-a325-f3709f8735f4>
2007-01-05 11:35:19 +0000

    macro OK
    git-svn-id: http://svn.momonga-linux.org/svn/pkgs/trunk/tools@13437 04999e60-30d4-0310-a325-f3709f8735f4

--- abc/src/mo_specparse.py
+++ abc/src/mo_specparse.py
@@ -3,6 +3,8 @@
 class spec_contents:
     def __init__(self, specfile):
         self.specfile = specfile
+        self.macro_key = ['%global', '%define']
+        self.macro = {}
         self.Tag = {'Name':'',
                     'Version':'',
                     'Release':''}
@@ -12,9 +14,18 @@ class spec_contents:
         spec.close()
         content_lines = content.splitlines()
         for line in content_lines:
+            for key in self.macro_key:
+                if line.startswith(key):
+                    self.macro[line.split()[1].strip()] = \
+                        line.split()[2].strip()
+        for line in content_lines:
             for key in self.Tag.iterkeys():
                 if line.startswith(key):
-                    self.Tag[key] = line.split(':')[1].lstrip().rstrip()
+                    self.Tag[key] = line.split(':')[1].strip()
+        for item in self.Tag.iterkeys():
+            for key in self.macro.iterkeys():
+                self.Tag[item] = \
+                    self.Tag[item].replace('%{' + key + '}',self.macro[key])
 
 if __name__ == "__main__":
     import sys
