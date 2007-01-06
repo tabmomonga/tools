b9400ed034d61c6a648caa320189ae5ded1cf59f
futoshi <futoshi@04999e60-30d4-0310-a325-f3709f8735f4>
2007-01-06 12:42:22 +0000

    use class
    git-svn-id: http://svn.momonga-linux.org/svn/pkgs/trunk/tools@13454 04999e60-30d4-0310-a325-f3709f8735f4

--- abc/src/mo_get_srpm_name.py
+++ abc/src/mo_get_srpm_name.py
@@ -1,25 +1,68 @@
 #! /bin/env python
 import rpm
+source = 1
+patch = 2
+noSource = 9
+noPatch = 10
 
-def getSrpmName(specFileName):
-    ts = rpm.ts()
-    spec = ts.parseSpec(specFileName)
-    header = spec.header()
-    soueces = spec.sources()
-    suffix = '.src.rpm'
-    for src in soueces:
-        if src[2] == 9:
-            suffix = '.nosrc.rpm'
-    srpmName = header[rpm.RPMTAG_NAME] + '-' + \
-        header[rpm.RPMTAG_VERSION] + '-' + \
-        header[rpm.RPMTAG_RELEASE] + \
-        suffix
-    return srpmName
+tag_filename = 0
+tag_category = 2
+# 0: Source
+# 1: Patch
+# 9: NoSource
+#10: NoPatch
+
+class spec:
+    def __init__(self, specFileName):
+        self.specFileName = specFileName
+        ts = rpm.ts()
+        self.spec = ts.parseSpec(specFileName)
+        self.header = self.spec.header()
+        self.sources = self.spec.sources()
+    def getSrpmName(self):
+        suffix = '.src.rpm'
+        for src in self.sources:
+            if src[tag_category] == noSource:
+                suffix = '.nosrc.rpm'
+        srpmName = self.header[rpm.RPMTAG_NAME] + '-' + \
+            self.header[rpm.RPMTAG_VERSION] + '-' + \
+            self.header[rpm.RPMTAG_RELEASE] + \
+            suffix
+        return srpmName
+    def getSources(self):
+        sources = []
+        for src in self.sources:
+            if src[tag_category] == source:
+                sources.append(src[tag_filename].split('/')[-1])
+        return sources
+    def getNoSources(self):
+        noSources = []
+        for src in self.sources:
+            if src[tag_category] == noSource:
+                noSources.append(src[tag_filename])
+        return noSources
+    def getPatches(self):
+        patches = []
+        for src in self.sources:
+            if src[tag_category] == patch:
+                patches.append(src[tag_filename].split('/')[-1])
+        return patches
+    def getNoPatches(self):
+        noPatches = []
+        for src in self.sources:
+            if src[tag_category] == noPatch:
+                noPatches.append(src[tag_filename])
+        return noPatches
 
 if __name__ == "__main__":
     import sys
-
+    spec = spec(sys.argv[1])
     try:
-        print getSrpmName(sys.argv[1])
+        print spec.getSrpmName()
+        print spec.getSources()
+        print spec.getNoSources()
+        print spec.getPatches()
+        print spec.getNoPatches()
     except:
         print "USAGE: ./mo_get_srpm_name.py specfile"
+
