94f8f9b069d330948354d1c38486d432e0f42648
futoshi <futoshi@04999e60-30d4-0310-a325-f3709f8735f4>
2007-01-13 00:52:20 +0000

    comment out options
    git-svn-id: http://svn.momonga-linux.org/svn/pkgs/trunk/tools@13571 04999e60-30d4-0310-a325-f3709f8735f4

--- abc/src/optionParse.py
+++ abc/src/optionParse.py
@@ -20,75 +20,81 @@ class optionParse:
         usage = _("usage:\n\t") + \
             DEFFILE.PACKAGE + "\t[option] packages"
         parser = OptionParser(usage=usage, version=DEFFILE.VERSION)
-        parser.add_option("-a", "--archdep",
-                          default=False, action="store_true", dest="noarch",
-                          help=_("ignore noarch packages"))
-        parser.add_option("-A", "--arch", metavar="ARCH",
-                          dest="arch",
-                          help=_("specify architecture"))
-        parser.add_option("-c", "--cvs",
-                          default=False, action="store_true", dest="cvs",
-                          help="ignored. remained for compatibility")
-        parser.add_option("-d", "--depend", metavar="DEPENDS",
-                          dest="depends",
-                          help=_("specify dependencies"))
-        parser.add_option("-f", "--force",
-                          default=False, action="store_true", dest="force",
-                          help=_("force build"))
-        parser.add_option("-g", "--checkgroup",
-                          default=False, action="store_true", dest="group",
-                          help=_("group check only"))
-        parser.add_option("-i", "--install",
-                          default=False, action="store_true", dest="install",
-                          help=_("force install after build (except kernel and usolame)"))
-        parser.add_option("-m", "--main",
-                          default=False, action="store_true", dest="main",
-                          help=_("main package only"))
+        # mandatory options
         parser.add_option("-n", "--nonfree",
                           default=False, action="store_true", dest="nonfree",
                           help=_("build Nonfree package, too"))
-        parser.add_option("-N", "--nostrict",
-                          default=False, action="store_true", dest="nostrict",
-                          help=_("proceed by old behavior"))
-        parser.add_option("-r", "--rpmopt", metavar="RPMOPTS",
-                          dest="rpmopt",
-                          help=_("specify option through to rpm"))
-        parser.add_option("-R", "--ignore-remove",
-                          default=False, action="store_true", dest="remove",
-                          help=_("do not uninstall packege if REMOVE.* exists"))
-        parser.add_option("-s", "--script",
-                          default=False, action="store_true", dest="script",
-                          help=_("script mode"))
-        parser.add_option("-S", "--scanpackages",
-                          default=False, action="store_true", dest="scanpackage",
-                          help=_("execute mph-scanpackage"))
-        parser.add_option("-v", "--verbose",
-                          default=False, action="store_true", dest="verbose",
-                          help=_("verbose mode"))
-        parser.add_option("-G", "--debug",
-                          default=False, action="store_true", dest="debug",
-                          help=_("enable debug flag"))
-        parser.add_option("-C", "--noccache",
-                          default=False, action="store_true", dest="noccache",
-                          help=_("no ccache"))
-        parser.add_option("-1", "--cachecc1",
-                          default=False, action="store_true", dest="cachecc1",
-                          help=_("use cachecc1"))
-        parser.add_option("-M", "--mirrorfirst",
-                          default=False, action="store_true", dest="mirrorfirst",
-                          help=_("download from mirror first"))
-        parser.add_option("-D", "--distcc",
-                          default=False, action="store_true", dest="distcc",
-                          help=_("enable to use distcc"))
         parser.add_option("-O", "--orphan",
                           default=False, action="store_true", dest="orphan",
                           help=_("build Orphan package, too"))
         parser.add_option("-L", "--alter",
                           default=False, action="store_true", dest="alter",
                           help=_("build Alter(alternative) package, too"))
-        parser.add_option("-z", "--zoo",
-                          default=False, action="store_true", dest="zoo",
-                          help=_("build Zoo package, too"))
+        # convenience options
+#        parser.add_option("-r", "--rpmopt", metavar="RPMOPTS",
+#                          dest="rpmopt",
+#                          help=_("specify option through to rpm"))
+#        parser.add_option("-f", "--force",
+#                          default=False, action="store_true", dest="force",
+#                          help=_("force build"))
+#        parser.add_option("-i", "--install",
+#                          default=False, action="store_true", dest="install",
+#                          help=_("force install after build (except kernel and usolame)"))
+#        parser.add_option("-s", "--script",
+#                          default=False, action="store_true", dest="script",
+#                          help=_("script mode"))
+#        parser.add_option("-v", "--verbose",
+#                          default=False, action="store_true", dest="verbose",
+#                          help=_("verbose mode"))
+#        parser.add_option("-G", "--debug",
+#                          default=False, action="store_true", dest="debug",
+#                          help=_("enable debug flag"))
+#        parser.add_option("-M", "--mirrorfirst",
+#                          default=False, action="store_true", dest="mirrorfirst",
+#                          help=_("download from mirror first"))
+
+        # ignored options
+#        parser.add_option("-m", "--main",
+#                          default=False, action="store_true", dest="main",
+#                          help=_("main package only"))
+#        parser.add_option("-N", "--nostrict",
+#                          default=False, action="store_true", dest="nostrict",
+#                          help=_("proceed by old behavior"))
+#        parser.add_option("-R", "--ignore-remove",
+#                          default=False, action="store_true", dest="remove",
+#                          help=_("do not uninstall packege if REMOVE.* exists"))
+#        parser.add_option("-S", "--scanpackages",
+#                          default=False, action="store_true", dest="scanpackage",
+#                          help=_("execute mph-scanpackage"))
+#        parser.add_option("-C", "--noccache",
+#                          default=False, action="store_true", dest="noccache",
+#                          help=_("no ccache"))
+#        parser.add_option("-1", "--cachecc1",
+#                          default=False, action="store_true", dest="cachecc1",
+#                          help=_("use cachecc1"))
+#        parser.add_option("-D", "--distcc",
+#                          default=False, action="store_true", dest="distcc",
+#                          help=_("enable to use distcc"))
+
+        # obsolete options
+#        parser.add_option("-a", "--archdep",
+#                          default=False, action="store_true", dest="noarch",
+#                          help=_("ignore noarch packages"))
+#        parser.add_option("-A", "--arch", metavar="ARCH",
+#                          dest="arch",
+#                          help=_("specify architecture"))
+#        parser.add_option("-c", "--cvs",
+#                          default=False, action="store_true", dest="cvs",
+#                          help="ignored. remained for compatibility")
+#        parser.add_option("-d", "--depend", metavar="DEPENDS",
+#                          dest="depends",
+#                          help=_("specify dependencies"))
+#        parser.add_option("-g", "--checkgroup",
+#                          default=False, action="store_true", dest="group",
+#                          help=_("group check only"))
+#        parser.add_option("-z", "--zoo",
+#                          default=False, action="store_true", dest="zoo",
+#                          help=_("build Zoo package, too"))
         return parser.parse_args()
 
 
