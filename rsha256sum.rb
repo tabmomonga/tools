3e0a029df304c70eb27cd97f85f7092db7fc48c7
smbd <smbd@04999e60-30d4-0310-a325-f3709f8735f4>
2008-03-29 17:34:42 +0000

    get remote file's sha256sum tool
    git-svn-id: http://svn.momonga-linux.org/svn/pkgs/trunk/tools@23109 04999e60-30d4-0310-a325-f3709f8735f4

---
+++ rsha256sum.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# get sha256sum via network, directly
+# you can get local file's sha256sum, too
+
+# Usage: rsha256sum.rb URI1(file1) URI2(file2) ...
+
+require 'open-uri'
+require 'digest/sha2'
+
+if ARGV.size == 0
+  puts "Usage: rsha256sum.rb URI1(file1) URI2(file2) ..."
+end
+
+while uri = ARGV.shift
+	open(uri) { |f|
+		sha256sum = Digest::SHA256.hexdigest(f.read)
+		puts "#{sha256sum}  #{uri}"
+	}
+end
