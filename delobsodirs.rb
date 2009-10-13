9ceb086133e31a3bf4f979fac139d3fff7ccc472
nakaya <nakaya@04999e60-30d4-0310-a325-f3709f8735f4>
2009-10-13 12:22:18 +0000

    * add --noworkdir option * add delobsodirs.rb * momorel must be an integer
    git-svn-id: http://svn.momonga-linux.org/svn/pkgs/trunk/tools@37726 04999e60-30d4-0310-a325-f3709f8735f4

---
+++ delobsodirs.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/ruby
+
+obso_dirs = []
+obso_files = []
+
+Dir.entries('.').each { |d|
+  if FileTest.directory?(d) \
+    && d != '.' \
+    && d != '..' \
+    && d != '.svn' \
+    && !FileTest.exist?(d + '/.svn') \
+    && !FileTest.exist?(d + '/' + d + '.spec') \
+  then
+    obso_dirs << d
+    obso_files << d
+    Dir.entries(d).each { |f|
+      obso_files << d + '/' + f if f != '.' && f != '..'
+    }
+  end
+}
+
+exit 0 if obso_dirs.empty?
+
+obso_files.sort.each { |f|
+  print "#{f}\n"
+}
+
+print "けすよ。(y/N)> "
+if STDIN.gets().chomp().downcase() != 'y' then
+  print "じゃ、けさない。\n"
+  exit 0
+end
+
+obso_dirs.each { |d|
+  system "rm -rf #{d}"
+}
